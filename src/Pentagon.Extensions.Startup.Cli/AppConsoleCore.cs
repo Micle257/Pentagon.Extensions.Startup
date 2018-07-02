ac409faffa2a74e6d17d34c52bda90ee1162ffcf
Michal Pokorný <michal.pokorny0@gmail.com>
2018-07-02 18:58:49 +0200

    added project Pentagon.Extensions.Startup.Cli

---
+++ src/Pentagon.Extensions.Startup.Cli/AppConsoleCore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Pentagon.Extensions.Startup.Cli
+{
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Utilities.Console.Helpers;
+
+    public abstract class AppConsoleCore : AppCore
+    {
+        public async Task RunCommand<TCommand, TOptions>(TOptions options, Action failCallback)
+                where TCommand : ICliCommand<TOptions>
+        {
+            try
+            {
+                var command = Services.GetService<TCommand>();
+
+                await command.RunAsync(options);
+            }
+            catch (Exception e)
+            {
+                Services.GetService<ILogger>()?.LogCritical(e, message: "Error while running play command.");
+                failCallback();
+                throw;
+            }
+
+        }
+
+        public virtual void OnExit(bool success)
+        {
+            if (Environment.Name == ApplicationEnvironmentNames.Development)
+            {
+                Console.WriteLine();
+                if (!success)
+                {
+                    ConsoleHelper.WriteError(errorValue: "Program execution failed.");
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine(value: " Press any key to exit the application...");
+                Console.ReadKey();
+            }
+            else if (Environment.Name == ApplicationEnvironmentNames.Production && !success)
+            {
+                ConsoleHelper.WriteError(errorValue: "Program execution failed.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ src/Pentagon.Extensions.Startup.Cli/ICliCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Pentagon.Extensions.Startup.Cli {
+    using System.Threading.Tasks;
+
+    public interface ICliCommand<in TOptions>
+    {
+        Task<int> RunAsync(TOptions options);
+    }
+}
