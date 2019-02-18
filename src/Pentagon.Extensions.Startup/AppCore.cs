cad638b23e8a58637ee997a8e57ee98979369e9e
Michal Pokorný <michal.pokorny0@gmail.com>
2019-02-18 17:31:58 +0100

    AppCore - added lock for OnStartup mtd

--- src/Pentagon.Extensions.Startup/AppCore.cs
+++ src/Pentagon.Extensions.Startup/AppCore.cs
@@ -1,4 +1,11 @@
-﻿namespace Pentagon.Extensions.Startup {
+﻿// -----------------------------------------------------------------------
+//  <copyright file="AppCore.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Extensions.Startup
+{
     using System;
     using System.Threading.Tasks;
     using Logging;
@@ -6,41 +13,60 @@
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
 
+    /// <summary> Represents a helper base class for app configuration. Similar to ASP.NET Core 'Startup' class. </summary>
     public abstract class AppCore
     {
-		IApplicationBuilder _builder;
-		
-        public IServiceProvider Services { get; private set; }
+        readonly object _lock = new object();
 
-        public IApplicationEnvironment Environment => _builder.Environment;
+        IApplicationBuilder _builder;
 
-        public IConfiguration Configuration => _builder.Configuration;
+        /// <summary> The value indicates if the <see cref="OnStartup" /> methods was called. </summary>
+        bool _startupCalled;
 
-        protected bool StartupCalled { get; private set; }
+        /// <summary> Gets the configuration instance. </summary>
+        /// <value> The <see cref="IConfiguration" />. </value>
+        public IConfiguration Configuration => _builder?.Configuration;
 
+        protected IApplicationEnvironment Environment => _builder?.Environment;
+
+        protected IServiceProvider Services { get; private set; }
+
+        /// <summary> Starts the startup procedure, configures the DI container. </summary>
+        /// <param name="args"> The program arguments. </param>
         public void OnStartup(string[] args)
         {
-            if (StartupCalled)
-                return;
+            lock (_lock)
+            {
+                if (_startupCalled)
+                    return;
 
-            StartupCalled = true;
+                try
+                {
+                    _builder = new ApplicationBuilder();
 
-            _builder = new ApplicationBuilder();
+                    _builder.AddLogging()
+                            .AddCommandLineArguments(args);
 
-            _builder.AddLogging()
-                   .AddCommandLineArguments(args);
+                    BuildApp(_builder, args);
 
-            BuildApp(_builder, args);
-                        
-            var result = _builder.Build();
+                    var result = _builder.Build();
 
-            Services = result.Provider;
-            
-            result.ApplyLogMessages(Services.GetService<ILogger>());
+                    Services = result.Provider;
 
-            ConfigureUnhandledExceptionHandling();
+                    result.ApplyLogMessages(Services.GetService<ILogger>());
+
+                    ConfigureUnhandledExceptionHandling();
+                }
+                finally
+                {
+                    _startupCalled = true;
+                }
+            }
         }
-        
+
+        /// <summary> Builds the application. </summary>
+        /// <param name="appBuilder"> The application builder. </param>
+        /// <param name="args"> The program arguments. </param>
         protected abstract void BuildApp(IApplicationBuilder appBuilder, string[] args);
 
         void ConfigureUnhandledExceptionHandling()
@@ -49,13 +75,13 @@
                                                           {
                                                               Services.GetService<ILogger>()?.LogSource(LogLevel.Error,
                                                                                                         message: "Exception unhandled (AppDomain).",
-                                                                                                        eventId: new EventId(),
-                                                                                                        exception: args.ExceptionObject as Exception);
+                                                                                                        new EventId(),
+                                                                                                        args.ExceptionObject as Exception);
                                                           };
 
             TaskScheduler.UnobservedTaskException += (sender, args) =>
                                                      {
-                                                         Services.GetService<ILogger>()?.LogSource(LogLevel.Error, message: "Exception unhandled (TaskScheduler).", eventId: new EventId(), exception: args.Exception);
+                                                         Services.GetService<ILogger>()?.LogSource(LogLevel.Error, message: "Exception unhandled (TaskScheduler).", new EventId(), args.Exception);
                                                      };
         }
     }
