3a7e307f3385cf833b27255baf4dbd8e16f6afae
Michal Pokorný <michal.pokorny0@gmail.com>
2019-08-05 19:12:40 +0200

    demo update

--- test/Pentagon.Extensions.Startup.CLI.TestApp/FirstCommand.cs
+++ test/Pentagon.Extensions.Startup.CLI.TestApp/FirstCommand.cs
@@ -1,9 +1,11 @@
-﻿namespace Pentagon.Extensions.Startup.CLI.TestApp {
+﻿namespace Pentagon.Extensions.Startup.CLI.TestApp
+{
     using System;
     using System.Threading;
     using System.Threading.Tasks;
     using Cli;
     using Microsoft.Extensions.Options;
+    using Threading;
 
     public class FirstCommand : CliHandler<FirstOptions>
     {
@@ -20,15 +22,25 @@
         }
 
         /// <inheritdoc />
-        public override async Task<int> RunAsync(FirstOptions options, CancellationToken cancellationToken = default)
+        public override async Task<int> RunAsync(FirstOptions options, CancellationToken cancellationToken)
         {
             Program.App.UpdateOptions<LolOptions>(o => { o.Lol = "first"; });
             Program.App.UpdateOptions<LolOptions>(o => { o.Lol = "second"; });
             Program.App.UpdateOptions<LolOptions>(o => { o.Lol = null; });
 
-            await Task.Delay(Timeout.InfiniteTimeSpan, cancellationToken);
+            try
+            {
+                await Task.Delay(Timeout.InfiniteTimeSpan, cancellationToken);
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine("Wait bitch");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                Console.WriteLine("good");
+                return 2;
+            }
 
-            return 1;
+            return 0;
         }
     }
 }
